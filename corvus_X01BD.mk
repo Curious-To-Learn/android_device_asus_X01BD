e4fa45c9857a066821658404a606e36869a502c6
Curious-To-Learn <kunmun.devroms@gmail.com>
2021-01-20 20:21:32 +0000

    TITANIUM ---> CORVUS

--- corvus_X01BD.mk
+++ corvus_X01BD.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2019-2020 The TitaniumOS Project
+# Copyright (C) 2020-2021 The CorvusOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,14 +19,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common Titanium stuff
-$(call inherit-product, vendor/titanium/config/common_full_phone.mk)
+# Inherit some common Corvus stuff
+$(call inherit-product, vendor/corvus/config/common_full_phone.mk)
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
-
-# Official Titanium
-Titanium_BUILD_TYPE := UNOFFICIAL
 
 # Google Connectivity Services
 TARGET_INCLUDE_WIFI_EXT := true
@@ -35,7 +32,7 @@ TARGET_INCLUDE_WIFI_EXT := true
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := titanium_X01BD
+PRODUCT_NAME := corvus_X01BD
 PRODUCT_DEVICE := X01BD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := ASUS_X01BDA
@@ -53,5 +50,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 # Build fingerprint
 BUILD_FINGERPRINT := "google/coral/coral:11/RQ1A.210105.003/7005429:user/release-keys"
 
-# Maintainer
-TITANIUM_BUILD_MAINTAINER := 🔥Curious_To_Learn🔥
